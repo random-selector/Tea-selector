c2599297002bceb28031f5bf9d54bb67fbbf46d6
random-selector <tukikanagi.24@gmail.com>
2025-08-26 02:36:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tea-choice-v7';
+const CACHE='tea-choice-v1';
 const ASSETS=['/','./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
