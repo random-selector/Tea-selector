bb473c62a3b162eecc70584ee3b429ed078602a2
random-selector <tukikanagi.24@gmail.com>
2025-08-26 02:27:09 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='tea-choice-v7';
+const ASSETS=['/','./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    if(e.request.method==='GET'){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}
+    return res;
+  }).catch(()=>cached||new Response('オフラインです',{status:200}))));
+});
